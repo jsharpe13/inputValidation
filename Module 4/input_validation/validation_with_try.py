74e8ba86105bf0f1575380491a2970e13a42418d
jsharpe13 <58717569+jsharpe13@users.noreply.github.com>
2020-06-10 19:32:03 -0500

    2nd test success

--- Module 4/input_validation/validation_with_try.py
+++ Module 4/input_validation/validation_with_try.py
@@ -1,6 +1,6 @@
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
-    if score1 < 0:
+    if score1 < 0 or score2 < 0:
         raise ValueError
 
     return float((score1 + score2 + score3) / NUMBER_TESTS)
