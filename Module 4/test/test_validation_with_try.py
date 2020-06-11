439cbc0b5cbe722d95f3e509b0f00ed0b759d1f2
jsharpe13 <58717569+jsharpe13@users.noreply.github.com>
2020-06-10 19:33:22 -0500

    3rd test fail

--- Module 4/test/test_validation_with_try.py
+++ Module 4/test/test_validation_with_try.py
@@ -6,10 +6,14 @@ class FunctionTestCase(unittest.TestCase):
     def test_average_exception(self):
         with self.assertRaises(ValueError):
             average(-90, 89, 78)
+
     def test_average_exceptionTwo(self):
         with self.assertRaises(ValueError):
             average(90, -89, 78)
 
+    def test_average_exceptionThree(self):
+        with self.assertRaises(ValueError):
+            average(90, 89, -78)
 
 if __name__ == '__main__':
     unittest.main()
